f6006eb433c2616544186c52dc7f191c70ba1ae8
yusufkorkmaz <32537154+yusufkorkmaz@users.noreply.github.com>
2018-12-08 16:46:41 +0200

    Bazı String ve Liste Metodları

---
+++ app7.py
@@ -0,0 +1,19 @@
+yazi = "app6"
+print(yazi.startswith("ap")) # String'in " " içinde yazdığımız string ile başlayıp başlamadığını
+print(yazi.startswith("ba")) # true yada false olarak geri döndürüyor.
+
+print(yazi.endswith("p6")) # Yukardaki metodun tam tersini gerçekleştirir.
+print(yazi.endswith("ta"))
+
+print(yazi.replace("p","e")) # Stringteki p yazan yerleri e olarak değiştirdik.
+print(yazi.replace("ap","pa")) # Stringteki ap yazan yerleri pa olarak değiştirdik.
+
+
+liste = [1,2,3,4,5,6]
+liste.append(7) # append , listenin sonuna bir eleman eklememizi sağlar.
+liste.append("txt")
+print(liste)
+
+liste.pop(0) # pop , içine yazılan index'i listeden siler
+liste.pop() # pop'un içi boş bırakılırsa sondaki elemanı siler
+print(liste)
