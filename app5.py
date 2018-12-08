39843a5216044247b89b26665860edc602b8ba76
yusufkorkmaz <32537154+yusufkorkmaz@users.noreply.github.com>
2018-12-08 15:17:55 +0200

    For Döngüsü

---
+++ app5.py
@@ -0,0 +1,14 @@
+metin = "app5"
+for i in metin:
+    print(i)
+
+print()
+
+elemanlar = [1,2,3,4,5]
+for x in elemanlar:
+    print(x)
+
+print()
+
+for sayi in range(1,11,3): # range özel bir metoddur. Soldaki sayıdan başlayıp ortadaki sayıya kadar olan sayıları yazar.
+    print(sayi)            # en sağdaki sayı ise artış miktarıdır.
